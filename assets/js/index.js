08671a7508ca43f4f077fbae352448f69888f94a
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2019-08-17 10:18:13 +0530

    Added prompt to delete all notes and view note functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,47 @@ function setup() {
   }
 }
 
-function clearStorage(notes = true) {
+function prompt(_notes = true, _prompt) {
+  inner = `
+  <div class="col-md-4">
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModal" aria-hidden="true">
+      <div class="modal-dialog modal-danger modal-dialog-centered modal-" role="document">
+          <div class="modal-content bg-gradient-danger">
+            
+              <div class="modal-header">
+                  <h3 class="modal-title" id="modal-title-notification">Attention!</h3>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                  </button>
+              </div>
+              
+              <div class="modal-body">
+                
+                  <div class="py-3 text-center">
+                      <i class="ni ni-bell-55 ni-3x"></i>
+                      <h4 class="heading mt-4">Are you sure want to delete all of your notes?</h4>
+                  </div>
+                  
+              </div>
+              
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-white" onclick="clearStorage();" data-dismiss="modal">Yes, delete all</button>
+                  <button type="button" class="btn btn-link text-white ml-auto" data-dismiss="modal">Close</button> 
+              </div>  
+          </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  document.getElementById("delete-modal").innerHTML = inner;
+  $("#deleteModal").modal();
+}
+
+function clearStorage(_notes=true) {
   localStorage.clear();
   document.getElementById("my-notes").innerHTML = `My Notes - 0`;
-  if (notes) {
+  if (_notes) {
     showNotes();
   }
 }
@@ -28,8 +65,8 @@ function showNotes() {
                 <h2 class="card-title">Note ${index + 1}</h2>
                 <p class="card-text">${element.slice(0, 120)}</p>
                 <div class="card-footer">
-                    <a href="" class="btn btn-primary" style="float: left;" data-toggle="modal" data-target="#delete-all-notification">View</a>
-                    <button class="btn btn-danger" id="${index}" style="float: right"; onclick="deleteNote(this.id);">Delete</button>
+                    <button class="btn btn-primary" id="${index}" style="float: left;" onclick="viewNote(this.id);"> View </button>
+                    <button class="btn btn-danger" id="${index}" style="float: right"; onclick="deleteNote(this.id);"> Delete </button>
                 </div>
             </div>
         </div>
@@ -70,6 +107,36 @@ function deleteNote(index) {
   localStorage.setItem("notes", JSON.stringify({ notes: notes }));
   document.getElementById("my-notes").innerHTML = `My Notes - ${notes.length}`;
   showNotes();
+}
+
+function viewNote(_id) {
+  console.log(_id);
+  modalBody = JSON.parse(localStorage.getItem("notes")).notes[_id];
+  let inner = `
+  <div class="col-md-4">
+      <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="viewModal" aria-hidden="true">
+      <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h3 class="modal-title" id="modal-title-default">Note ${_id}</h3>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">×</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+                <p>${modalBody}</p>
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-success ml-auto" data-dismiss="modal">Close</button> 
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+  `;
+
+  document.getElementById(`view-modal`).innerHTML = inner;
+  $("#viewModal").modal();
 }
 
 setup();
